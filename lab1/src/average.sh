b3d2900fcdef4f80a18e71b1725150a45458c910
N1gaz <58673572+N1gaz@users.noreply.github.com>
2020-03-05 16:47:52 +0300

    Add files via upload

--- lab1/src/average.sh
+++ lab1/src/average.sh
@@ -1,1 +1,17 @@
-for 
+sum=0
+count=0
+for i in $@
+do
+let sum=sum+$i
+let count=count+1
+done
+
+if [ $count -eq 0 ]
+then
+echo Arguments are empty set
+else
+echo Count $count
+echo Sum $sum
+let avg=sumcount
+echo Average $avg
+fi
