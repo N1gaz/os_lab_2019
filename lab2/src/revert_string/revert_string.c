ce4e651aec7e8de29b2611b3ecbc0f76c5fe6169
super.dartysh <super.dartysh@gmail.com>
2020-05-15 15:52:40 +0000

    The joke got out of hand

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,25 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	 int i = 0;
+     int end;
+
+     while(str[i] != '\0')
+     {
+         i++;
+     }
+
+     end = i - 1;
+     i = 0;
+
+    char buff;
+
+     while(i < end/2)
+     {
+         buff = str[i];
+         str[i] = str[end - i];
+         str[end - i] = buff;
+         i++;
+     }
 }
 
